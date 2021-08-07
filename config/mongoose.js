01462e19712fcef8bcdec6e12464c3df0d1d8507
johnson <poshenc@gmail.com>
2021-08-07 14:11:09 +0800

    modify todoSedder.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-
 const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
